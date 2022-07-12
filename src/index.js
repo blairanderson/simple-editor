948e692ea050106a4c74e1d3aa6e968058f2f73e
Blair Anderson <blair81@gmail.com>
2022-07-12 11:46:00 -0700

    update settings

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import * as markerjs2 from "markerjs2";
 
 function showMarkerArea(target) {
   const markerArea = new markerjs2.MarkerArea(target);
-  markerArea.availableMarkerTypes = ["FrameMarker", markerjs2.ArrowMarker];
+  markerArea.availableMarkerTypes = markerArea.ALL_MARKER_TYPES;
+  markerArea.settings.defaultStrokeWidths = [1, 3, 5, 10, 15];
+  markerArea.settings.defaultStrokeWidth = 5;
+  markerArea.settings.defaultStrokeDasharrays = ["", "3", "6 3", "12 3"];
+  markerArea.settings.defaultColor = "#000000";
+  markerArea.settings.defaultColorSet = [
+    "#EF4444", // red
+    "#10B981", // green
+    "#2563EB", // blue
+    "#FFFF00", // yellow
+    "#7C3AED", // purple
+    "#F472B6", // pink
+    "#000000", // black
+    "#FFFFFF" //white
+  ];
+  markerArea.settings.defaultColorsFollowCurrentColors = true;
+  markerArea.settings.defaultColorsFollowCurrentColors = true;
   markerArea.addEventListener(
     "render",
     (event) => (target.src = event.dataUrl)
